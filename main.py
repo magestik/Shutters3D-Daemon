0af57b0af08ba6157b8a401369b1c20797f580c9
Magestik <bastien@magestik.fr>
2010-09-18 20:43:43 +0200

    Threading + Systray Icon + Notifications

--- main.py
+++ main.py
@@ -2,30 +2,43 @@
 # -*- coding: utf-8 -*-
 
 import sys, time
+import pynotify
+import wx
 
 #import nv3d
 import socket
 
-class daemon:
-	def __init__(self):
-		#self.glasses = nv3d.shutters()
-		#self.glasses.set_rate(120)
+from threading import Thread
+import gobject
+gobject.threads_init() # For prevent GTK freeze
+
+class daemon(Thread):
+	def __init__(self,i): # Prepare the socket in a separated Thread
+		Thread.__init__(self)
 		
 		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.socket.bind(("", 5000))
+		self.socket.bind(('', 5000))
 		self.socket.listen(5)
+
+		self.gui = i
+		self.status = -1 # for the Thread
 	
-	def wait(self):
+	def run(self): # Is called when we start the Thread
 		while 1:
-			self.client, address = self.socket.accept()
+			self.client, address = self.socket.accept() # Wait here
 			print "Connection from ", address
-			#self.client.send(data)
 			self.synchro()
 
-	def synchro(self):
+	def synchro(self): # Listen the client for controlling glasses
+		#self.glasses = nv3d.shutters()
+		#self.glasses.set_rate(120)
+		self.gui.notify("3D signal detected")
+		
+		#self.client.send(data) # to send some informations to the client (maybe key status)
 		while 1:
 			data = self.client.recv(512)
 			if (data == 'q' or data == 'Q'):
+				self.gui.notify("3D signal terminated")
 				self.client.close()
 				break;
 			else:
@@ -36,8 +49,34 @@ class daemon:
 					#self.glasses.set_eye(1)
 					i=1
 				else:
-					print "ERROR"
+					self.gui.notify("Error : client has been killed ?")
+					self.client.close()
+					break;
+
+		#self.glasses.__del__() # Stop glasses and release interface
+
+class interface(wx.Frame):
+	def __init__(self, parent):
+		wx.Frame.__init__(self, parent, style=wx.FRAME_NO_TASKBAR | wx.NO_FULL_REPAINT_ON_RESIZE)
+		pynotify.init("Shutters3D Daemon")		
+		
+		self.tbicon = wx.TaskBarIcon()
+		icon = wx.Icon('3D.ico', wx.BITMAP_TYPE_ICO)
+		self.tbicon.SetIcon(icon, '')
+		
+		self.Show(True)
+
+   	def notify(self, msg):
+		msg = pynotify.Notification("Nvidia 3D Vision", msg, "3D.png")
+		msg.show()
 
 if __name__ == "__main__":
-	d = daemon()
-	d.wait()
+	app = wx.App(False)
+	
+	i = interface(None)
+	i.Show(False)
+	
+	d = daemon(i)
+	d.start()
+	
+	app.MainLoop()
